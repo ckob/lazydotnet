0ef898ae4065bbee2fe2ec9b8da0a6ba258e2e07
Charly Koch <charlykoch@gmail.com>
2026-02-10 19:50:19 +0100

    fix(ui): force UTF8 to able to render icons

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Build.Locator;
 using lazydotnet.Commands;
 using Spectre.Console.Cli;
 
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 MSBuildLocator.RegisterDefaults();
 
 var app = new CommandApp<DefaultCommand>();
